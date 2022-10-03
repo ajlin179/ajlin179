4ce36d7e0a1fd13ff4822cb00174470a2c9d9ed8
Alwin <90728388+ajlin179@users.noreply.github.com>
2022-10-03 14:59:48 -0700

    Update CpuIntensiveTask.java

--- CpuIntensiveTask.java
+++ CpuIntensiveTask.java
@@ -10,10 +10,21 @@ public class CpuUtilizationApp {
     public static void main(String[] args) throws Exception {
         Thread t1 =   new Thread(new CpuIntensiveTask("T1", 1000000));
         Thread t2 = new Thread(new CpuIntensiveTask("T2", 3000000));
+        t1.setName("cpuThread1");
+        t2.setName("cpuThread2");
         t1.start();
         t2.start();
+
+
+        Thread.currentThread().setName("MainCpuThread");
+        new CpuIntensiveTask("main", 2200001).run();
+        System.out.println("Done.."+ Thread.currentThread().getName());
+
         t1.join();
+        System.out.println("Done..T1");
         t2.join();
+
+        System.out.println("Done..T2");
 
         Thread.sleep(1000 * 60 * 1);
     }
@@ -30,6 +41,7 @@ public class CpuUtilizationApp {
 
         @Override
         public void run() {
+            System.out.println("Thread Name"+ Thread.currentThread().getName());
 
             while (loopCount-- > 0) {
                 int num = 30;
@@ -38,7 +50,7 @@ public class CpuUtilizationApp {
                     // factorial = factorial * i;
                     factorial = factorial.multiply(BigInteger.valueOf(i));
                 }
-                System.out.printf("%s Factorial of %d = %d", name, num, factorial);
+               // System.out.printf("%s Factorial of %d = %d", name, num, factorial);
             }
         }
     }
